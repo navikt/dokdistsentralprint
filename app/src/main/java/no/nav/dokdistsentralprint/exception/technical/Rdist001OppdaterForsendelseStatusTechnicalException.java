37971d1d7ffb5cca163298f6d84a66daf6d0907b
Sigurd Midttun <sigurd.midttun@nav.no>
2019-03-11 17:07:51 +0100

    MMA-2079 Noen filer kom ikke med i forrige commit

---
+++ app/src/main/java/no/nav/dokdistsentralprint/exception/technical/Rdist001OppdaterForsendelseStatusTechnicalException.java
@@ -0,0 +1,15 @@
+package no.nav.dokdistsentralprint.exception.technical;
+
+/**
+ * @author Jarl Øystein Samseth, Visma Consulting
+ */
+public class Rdist001OppdaterForsendelseStatusTechnicalException extends AbstractDokdistsentralprintTechnicalException {
+
+	public Rdist001OppdaterForsendelseStatusTechnicalException(String message) {
+		super(message);
+	}
+
+	public Rdist001OppdaterForsendelseStatusTechnicalException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
