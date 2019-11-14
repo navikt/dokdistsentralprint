79bcc2b165d9246af37d651d383739a5be32e00d
Martin Burheim Tingstad <Martin.Burheim.Tingstad@nav.no>
2019-11-14 17:13:12 +0100

    MMA-3807: Fikset testdata og fjernet unødvendig kode

--- app/src/main/java/no/nav/dokdistsentralprint/qdist009/util/CDataCharacterEscapeHandler.java
+++ app/src/main/java/no/nav/dokdistsentralprint/qdist009/util/CDataCharacterEscapeHandler.java
@@ -1,7 +1,6 @@
 package no.nav.dokdistsentralprint.qdist009.util;
 
 import com.sun.xml.bind.marshaller.CharacterEscapeHandler;
-import com.sun.xml.bind.marshaller.MinimumEscapeHandler;
 
 import java.io.IOException;
 import java.io.Writer;
@@ -17,27 +16,7 @@ public class CDataCharacterEscapeHandler implements CharacterEscapeHandler {
 
 	@Override
 	public void escape(char[] ch, int start, int length, boolean isAttVal, Writer out) throws IOException {
-		boolean isCData = length > cdataPrefix.length + cDataSuffix.length;
-		if(isCData){
-			for(int i = 0, j = start; i < cdataPrefix.length; ++i , ++j) {
-				if (cdataPrefix[i] != ch[j]) {
-					isCData = false;
-					break;
-				}
-			}
-		}if(isCData){
-			for(int i = cDataSuffix.length -1, j = start + length -1; i >= 0; --i, --j) {
-				if (cDataSuffix[i] != ch[j]) {
-					isCData = false;
-					break;
-				}
-			}
-
-		}if(isCData){
-			String s = new String(ch).substring(start, start + length).replaceAll("\r", new String(chars));
-			out.write(s);
-		} else {
-			MinimumEscapeHandler.theInstance.escape(ch, start, length, isAttVal, out);
-		}
+		String s = new String(ch).substring(start, start + length).replaceAll("\r", new String(chars));
+		out.write(s);
 	}
 }
