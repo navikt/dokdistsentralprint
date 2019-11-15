ae11cfd8913fa025514ddc6cfa2a51586300a949
Olav Røstvold Thorsen <Olav.Rostvold.Thorsen@nav.no>
2019-11-15 07:48:11 +0100

    MMA-3807 Lagt til minimum escape handler for det som ikke er CDATA

--- app/src/main/java/no/nav/dokdistsentralprint/qdist009/util/CDataCharacterEscapeHandler.java
+++ app/src/main/java/no/nav/dokdistsentralprint/qdist009/util/CDataCharacterEscapeHandler.java
@@ -1,6 +1,7 @@
 package no.nav.dokdistsentralprint.qdist009.util;
 
 import com.sun.xml.bind.marshaller.CharacterEscapeHandler;
+import com.sun.xml.bind.marshaller.MinimumEscapeHandler;
 
 import java.io.IOException;
 import java.io.Writer;
@@ -10,11 +11,33 @@ import java.io.Writer;
  */
 public class CDataCharacterEscapeHandler implements CharacterEscapeHandler {
 
+	private static final char[] cdataPrefix = "<![CDATA[".toCharArray();
+	private static final char[] cDataSuffix = "]]>".toCharArray();
 	private static final char[] chars = {'\\', '\\', 'r'};
 
 	@Override
 	public void escape(char[] ch, int start, int length, boolean isAttVal, Writer out) throws IOException {
-		String s = new String(ch).substring(start, start + length).replaceAll("\r", new String(chars));
-		out.write(s);
+		boolean isCData = length > cdataPrefix.length + cDataSuffix.length;
+		if(isCData){
+			for(int i = 0, j = start; i < cdataPrefix.length; ++i , ++j) {
+				if (cdataPrefix[i] != ch[j]) {
+					isCData = false;
+					break;
+				}
+			}
+		}if(isCData){
+			for(int i = cDataSuffix.length -1, j = start + length -1; i >= 0; --i, --j) {
+				if (cDataSuffix[i] != ch[j]) {
+					isCData = false;
+					break;
+				}
+			}
+
+		}if(isCData){
+			String s = new String(ch).substring(start, start + length).replaceAll("\r", new String(chars));
+			out.write(s);
+		} else {
+			MinimumEscapeHandler.theInstance.escape(ch, start, length, isAttVal, out);
+		}
 	}
 }
