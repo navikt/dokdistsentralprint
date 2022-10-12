f8e71364ad7a45270d6438e3649ab18045d02c32
tsigab <tsigab.angosom@gmail.com>
2022-10-12 13:52:38 +0200

    MMA-6422 la til appdynamics og heap minne

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,4 @@ FROM navikt/java:17
 COPY app/target/app.jar /app/app.jar
 COPY export-vault-secrets.sh /init-scripts/50-export-vault-secrets.sh
 COPY dokdistsentralprint-java-opts.sh /init-scripts/51-dokdistsentralprint-java-opts.sh
+ENV APPD_ENABLED=true
--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -43,3 +43,12 @@ then
     echo "Setting DOKDISTSENTRALPRINTCERT_PASSWORD"
     export DOKDISTSENTRALPRINTCERT_PASSWORD=$(cat /var/run/secrets/nais.io/certificate/keystorepassword)
 fi
+
+echo "Exporting appdynamics environment variables"
+if test -f /var/run/secrets/nais.io/appdynamics/appdynamics.env;
+then
+    export $(cat /var/run/secrets/nais.io/appdynamics/appdynamics.env)
+    echo "Appdynamics environment variables exported"
+else
+    echo "No such file or directory found at /var/run/secrets/nais.io/appdynamics/appdynamics.env"
+fi
