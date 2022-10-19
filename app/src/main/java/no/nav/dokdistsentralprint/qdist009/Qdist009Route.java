f736446965cc1a25fc6107a0754e9355b7a6e063
tsigab <tsigab.angosom@gmail.com>
2022-10-19 13:36:03 +0200

    MMA-6422 satt concurrentConsumers til 2

--- app/src/main/java/no/nav/dokdistsentralprint/qdist009/Qdist009Route.java
+++ app/src/main/java/no/nav/dokdistsentralprint/qdist009/Qdist009Route.java
@@ -75,8 +75,7 @@ public class Qdist009Route extends SpringRouteBuilder {
                 .log(LoggingLevel.WARN, log, "${exception}; " + getIdsForLogging())
                 .to("jms:" + qdist009FunksjonellFeil.getQueueName());
 
-        from("jms:" + qdist009.getQueueName() +
-                "?transacted=true&concurrentConsumers=2&maxConcurrentConsumers=10")
+        from("jms:" + qdist009.getQueueName() + "?transacted=true&concurrentConsumers=2")
                 .routeId(SERVICE_ID)
                 .routePolicy(qdist009MetricsRoutePolicy)
                 .setExchangePattern(ExchangePattern.InOnly)
