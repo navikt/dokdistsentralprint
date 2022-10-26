43d3d0859eb3fc958ffac986c8bde7667b35cd66
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2022-10-26 12:15:44 +0200

    MMA-6425 La til konfig for nye ssh nøkler.
    * Oppdatert camel versjon til nyere sftp klient med støtte for nye nøkler.

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -12,16 +12,16 @@ then
     export SERVICEUSER_PASSWORD=$(cat /var/run/secrets/nais.io/srvdokdistsentralprint/password)
 fi
 
-if test -f /var/run/secrets/nais.io/privateKey/privateKeyFile;
+if test -f /var/run/secrets/nais.io/vault/sftp_privatekey_file
 then
     echo "Setting sftp_privateKeyFile"
-    export sftp_privateKeyFile=/var/run/secrets/nais.io/privateKey/privateKeyFile
+    export sftp_privateKeyFile=/var/run/secrets/nais.io/vault/sftp_privatekey_file
 fi
 
-if test -f /var/run/secrets/nais.io/privateKey/privateKeyPassphrase;
+if test -f /var/run/secrets/nais.io/vault/sftp_privatekey_passphrase
 then
     echo "Setting sftp_privateKeyPassphrase"
-    export sftp_privateKeyPassphrase=$(cat /var/run/secrets/nais.io/privateKey/privateKeyPassphrase)
+    export sftp_privateKeyPassphrase=$(cat /var/run/secrets/nais.io/vault/sftp_privatekey_passphrase)
 fi
 
 if test -f /var/run/secrets/nais.io/vault/gcloud_serviceaccount
