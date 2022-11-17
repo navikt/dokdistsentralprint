fbea744762bc5925a4f7b4048566dc71b62c5a01
Anders Opskar Voldsund <anders.voldsund@nav.no>
2022-11-17 09:07:43 +0100

    MMA-6429 Fjern ubrukt exception

--- app/src/main/java/no/nav/dokdistsentralprint/exception/functional/CryptoException.java
+++
@@ -1,8 +0,0 @@
-package no.nav.dokdistsentralprint.exception.functional;
-
-public class CryptoException extends AbstractDokdistsentralprintFunctionalException {
-
-	public CryptoException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}
