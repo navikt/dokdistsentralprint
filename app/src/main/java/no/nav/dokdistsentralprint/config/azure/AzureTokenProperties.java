0c9a111384badcda071e76640233eaf82d469d18
Anders Opskar Voldsund <anders.voldsund@nav.no>
2024-06-26 11:15:00 +0200

    MMA-7477 Bytt ut getOAuth2AuthorizedClient med clientRegistrationId

--- app/src/main/java/no/nav/dokdistsentralprint/config/azure/AzureTokenProperties.java
+++ app/src/main/java/no/nav/dokdistsentralprint/config/azure/AzureTokenProperties.java
@@ -2,7 +2,6 @@ package no.nav.dokdistsentralprint.config.azure;
 
 import jakarta.validation.constraints.NotEmpty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
@@ -12,14 +11,7 @@ public record AzureTokenProperties(
 		@NotEmpty String appClientId,
 		@NotEmpty String appClientSecret
 ) {
-	public static final String SPRING_DEFAULT_PRINCIPAL = "anonymousUser";
+
 	public static final String CLIENT_REGISTRATION_DOKDISTADMIN = "azure-dokdistadmin";
 	public static final String CLIENT_REGISTRATION_REGOPPSLAG = "azure-regoppslag";
-
-	public static OAuth2AuthorizeRequest getOAuth2AuthorizeRequestForAzure(String clientRegistrationId) {
-		return OAuth2AuthorizeRequest
-				.withClientRegistrationId(clientRegistrationId)
-				.principal(SPRING_DEFAULT_PRINCIPAL)
-				.build();
-	}
 }
