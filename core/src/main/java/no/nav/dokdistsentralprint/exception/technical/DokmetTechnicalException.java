e8158579d9d1485f8e7c18c55a7955e390900b8a
tsigab <tsigab.angosom@gmail.com>
2026-09-18 12:55:26 +0200

    MMA-8864 fiks etter PR tilbakemeldinger

--- core/src/main/java/no/nav/dokdistsentralprint/exception/technical/DokmetTechnicalException.java
+++ core/src/main/java/no/nav/dokdistsentralprint/exception/technical/DokmetTechnicalException.java
@@ -1,7 +1,7 @@
 package no.nav.dokdistsentralprint.exception.technical;
 
 public class DokmetTechnicalException extends AbstractDokdistsentralprintTechnicalException {
-	public DokmetTechnicalException(String message) {
-		super(message);
+	public DokmetTechnicalException(String message, Throwable cause) {
+		super(message, cause);
 	}
 }
